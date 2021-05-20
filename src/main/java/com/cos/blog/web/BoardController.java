3d8115f0f85ea7b1e8c3a9ced38f5ffca5f4d1d3
jungspin <soopinjj@gmail.com>
2021-05-20 11:46:47 +0900

    기능정리완료

--- src/main/java/com/cos/blog/web/BoardController.java
+++ src/main/java/com/cos/blog/web/BoardController.java
@@ -14,17 +14,43 @@ public class BoardController extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		doProcess(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		doProcess(request, response);
 	}
 
 	public void doProcess(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		
+		// 1. null과 공백 접근 금지 (유효성 검사)
+		if(request.getParameter("cmd")==null || request.getParameter("cmd").equals("") ) {
+			return;
+		}
+		
+		// 하나의 컨트롤러에서 다양한 요청을 받아 분기해주기
+		String cmd = request.getParameter("cmd"); 
+		
+		// http://localhost:8000/blog/board?cmd=list
+		if (cmd.equals("list")) {
+			
+		} else if (cmd.equals("detail")) { 
+			
+		} else if (cmd.equals("delete")) { 
+			
+		} else if (cmd.equals("updateForm")) { 
+			
+		} else if (cmd.equals("update")) {
+			
+		} else if (cmd.equals("saveForm")) {
+			
+		} else if (cmd.equals("save")) {
+			
+		} else if (cmd.equals("search")) {
+			
+		}
 	}
 
 }
--- src/main/java/com/cos/blog/web/UserController.java
+++ src/main/java/com/cos/blog/web/UserController.java
@@ -12,19 +12,42 @@ import javax.servlet.http.HttpServletResponse;
 public class UserController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-
-	}
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+			doProcess(request, response);
+		}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		doProcess(request, response);
 	}
 
 	public void doProcess(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
+		
+		// 1. null과 공백 접근 금지 (유효성 검사)
+		if(request.getParameter("cmd")==null || request.getParameter("cmd").equals("") ) {
+			return;
+		}
+		
+		// 하나의 컨트롤러에서 다양한 요청을 받아 분기해주기
+		String cmd = request.getParameter("cmd"); 
+		
+		if (cmd.equals("joinForm")) { // form 적혀있으면 전부 sendRedirect
+			
+		} else if (cmd.equals("join")) { // joinProc
+			
+		} else if (cmd.equals("loginForm")) { 
+			
+		} else if (cmd.equals("login")) {
+			
+		} else if (cmd.equals("updateForm")) {
+			
+		} else if (cmd.equals("update")) {
+			
+		} else if (cmd.equals("logout")) {
+			
+		}
 	}
 
 }
